6d726a699b4c27b6c2c27eddd009eb48437a98a4
02danieljonas <02danieljonas@gmail.com>
2023-01-12 19:11:48 -0500

    removed My First test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,9 +3,3 @@ describe("empty spec", () => {
         cy.visit("https://example.cypress.io");
     });
 });
-
-describe("My First Test", () => {
-    it("Does not do much!", () => {
-        expect(true).to.equal(false);
-    });
-});
