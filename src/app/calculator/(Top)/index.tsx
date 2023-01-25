ab3b6c02638b13803564e2e5659df05cd7f1160e
02danieljonas <02danieljonas@gmail.com>
2023-01-24 20:40:09 -0500

    Added a temp input

--- src/app/calculator/(Top)/index.tsx
+++ src/app/calculator/(Top)/index.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+"use client";
+
+//TODO modify querry url instead of a useState
+
+import React, { useState } from "react";
 
 export default function index() {
-    return <div>index</div>;
+    const [inputValues, setInputValues] = useState<string[]>([]);
+    return (
+        <div>
+            <input
+                type="text"
+                onChange={(e) => setInputValues([e.target.value])}
+            />
+            {inputValues}
+            index
+        </div>
+    );
 }
