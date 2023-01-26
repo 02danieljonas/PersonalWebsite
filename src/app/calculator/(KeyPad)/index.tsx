8f88b2a13eb309f27f3e9fca17baf1055395dfd2
02danieljonas <02danieljonas@gmail.com>
2023-01-26 11:14:24 -0500

    differences between face and value of Button

--- src/app/calculator/(KeyPad)/index.tsx
+++ src/app/calculator/(KeyPad)/index.tsx
@@ -5,32 +5,33 @@ type ButtonGridProps = {
 }
 
 export default function ButtonGrid({handleKeyPress}:ButtonGridProps) {
+
     const y = [
-        "7",
-        "8",
-        "9",
-        "y^x",
-        "del",
-        "4",
-        "5",
-        "6",
-        "*",
-        "/",
-        "1",
-        "2",
-        "3",
-        "+",
-        "-",
-        "+/-",
-        "0",
-        ".",
-        "c",
-        "="
+        ["7", "7"],
+        ["8", "8"],
+        ["9", "9"],
+        ["y^x", "^"],
+        ["del", "Delete"],
+        ["4", "4"],
+        ["5", "5"],
+        ["6", "6"],
+        ["*", "*"],
+        ["/", "/"],
+        ["1", "1"],
+        ["2", "2"],
+        ["3", "3"],
+        ["+", "+"],
+        ["-", "-"],
+        ["+/-", "+/-"],
+        ["0", "0"],
+        [".", "."],
+        ["C", "C"],
+        ["=", "="]
     ];
     return (
         <div className="grid gap-4 m-4 grid-cols-5 flex-grow">
-            {y.map((e) => (
-                <Button key={e} onClick={()=>handleKeyPress(e)} value={e} />
+            {y.map(([face, value]) => (
+                <Button key={face} onClick={()=>handleKeyPress(value)} value={face} />
             ))}
         </div>
     );
